4fb10922af78d3097f7dc6cb5d2c5285a536088b
Tomasz Tarnowski <t.tarnowski90@gmail.com>
2024-01-07 22:06:32 +0100

    end method task kill fix

--- src/ModbusClientRTU.cpp
+++ src/ModbusClientRTU.cpp
@@ -1,5 +1,6 @@
 // =================================================================================================
-// eModbus: Copyright 2020 by Michael Harwerth, Bert Melis and the contributors to eModbus
+// eModbus: Copyright 2020 by Michael Harwerth, Bert Melis and the contributors
+// to eModbus
 //               MIT license - see license.md for details
 // =================================================================================================
 #include "ModbusClientRTU.h"
@@ -11,40 +12,28 @@
 #include "Logging.h"
 
 // Constructor takes an optional DE/RE pin and queue size
-ModbusClientRTU::ModbusClientRTU(int8_t rtsPin, uint16_t queueLimit) :
-  ModbusClient(),
-  MR_serial(nullptr),
-  MR_lastMicros(micros()),
-  MR_interval(2000),
-  MR_rtsPin(rtsPin),
-  MR_qLimit(queueLimit),
-  MR_timeoutValue(DEFAULTTIMEOUT),
-  MR_useASCII(false),
-  MR_skipLeadingZeroByte(false) {
-    if (MR_rtsPin >= 0) {
-      pinMode(MR_rtsPin, OUTPUT);
-      MTRSrts = [this](bool level) {
-        digitalWrite(MR_rtsPin, level);
-      };
-      MTRSrts(LOW);
-    } else {
-      MTRSrts = RTUutils::RTSauto;
-    }
+ModbusClientRTU::ModbusClientRTU(int8_t rtsPin, uint16_t queueLimit)
+    : ModbusClient(), MR_serial(nullptr), MR_lastMicros(micros()),
+      MR_interval(2000), MR_rtsPin(rtsPin), MR_qLimit(queueLimit),
+      MR_timeoutValue(DEFAULTTIMEOUT), MR_useASCII(false),
+      MR_skipLeadingZeroByte(false) {
+  if (MR_rtsPin >= 0) {
+    pinMode(MR_rtsPin, OUTPUT);
+    MTRSrts = [this](bool level) { digitalWrite(MR_rtsPin, level); };
+    MTRSrts(LOW);
+  } else {
+    MTRSrts = RTUutils::RTSauto;
+  }
 }
 
 // Alternative constructor takes an RTS callback function
-ModbusClientRTU::ModbusClientRTU(RTScallback rts, uint16_t queueLimit) :
-  ModbusClient(),
-  MR_serial(nullptr),
-  MR_lastMicros(micros()),
-  MR_interval(2000),
-  MTRSrts(rts),
-  MR_qLimit(queueLimit),
-  MR_timeoutValue(DEFAULTTIMEOUT),
-  MR_useASCII(false),
-  MR_skipLeadingZeroByte(false) {
-    MR_rtsPin = -1;
-    MTRSrts(LOW);
+ModbusClientRTU::ModbusClientRTU(RTScallback rts, uint16_t queueLimit)
+    : ModbusClient(), MR_serial(nullptr), MR_lastMicros(micros()),
+      MR_interval(2000), MTRSrts(rts), MR_qLimit(queueLimit),
+      MR_timeoutValue(DEFAULTTIMEOUT), MR_useASCII(false),
+      MR_skipLeadingZeroByte(false) {
+  MR_rtsPin = -1;
+  MTRSrts(LOW);
 }
 
 // Destructor: clean up queue, task etc.
@@ -54,13 +43,13 @@ ModbusClientRTU::~ModbusClientRTU() {
 }
 
 // begin: start worker task - general version
-void ModbusClientRTU::begin(Stream& serial, uint32_t baudRate, int coreID) {
+void ModbusClientRTU::begin(Stream &serial, uint32_t baudRate, int coreID) {
   MR_serial = &serial;
   doBegin(baudRate, coreID);
 }
 
 // begin: start worker task - HardwareSerial version
-void ModbusClientRTU::begin(HardwareSerial& serial, int coreID) {
+void ModbusClientRTU::begin(HardwareSerial &serial, int coreID) {
   MR_serial = &serial;
   uint32_t baudRate = serial.baudRate();
   serial.setRxFIFOFull(1);
@@ -81,7 +70,8 @@ void ModbusClientRTU::doBegin(uint32_t baudRate, int coreID) {
   char taskName[18];
   snprintf(taskName, 18, "Modbus%02XRTU", instanceCounter);
   // Start task to handle the queue
-  xTaskCreatePinnedToCore((TaskFunction_t)&handleConnection, taskName, 4096, this, 6, &worker, coreID >= 0 ? coreID : NULL);
+  xTaskCreatePinnedToCore((TaskFunction_t)&handleConnection, taskName, 4096,
+                          this, 6, &worker, coreID >= 0 ? coreID : NULL);
 
   LOG_D("Client task %d started. Interval=%d\n", (uint32_t)worker, MR_interval);
 }
@@ -102,6 +92,7 @@ void ModbusClientRTU::end() {
     // Kill task
     vTaskDelete(worker);
     LOG_D("Client task %d killed.\n", (uint32_t)worker);
+    worker = nullptr;
   }
 }
 
@@ -125,9 +116,7 @@ void ModbusClientRTU::useModbusRTU() {
 }
 
 // Inquire protocol mode
-bool ModbusClientRTU::isModbusASCII() {
-  return MR_useASCII;
-}
+bool ModbusClientRTU::isModbusASCII() { return MR_useASCII; }
 
 // Toggle skipping of leading 0x00 byte
 void ModbusClientRTU::skipLeading0x00(bool onOff) {
@@ -136,13 +125,11 @@ void ModbusClientRTU::skipLeading0x00(bool onOff) {
 }
 
 // Return number of unprocessed requests in queue
-uint32_t ModbusClientRTU::pendingRequests() {
-  return requests.size();
-}
+uint32_t ModbusClientRTU::pendingRequests() { return requests.size(); }
 
 // Base addRequest taking a preformatted data buffer and length as parameters
 Error ModbusClientRTU::addRequestM(ModbusMessage msg, uint32_t token) {
-  Error rc = SUCCESS;        // Return value
+  Error rc = SUCCESS; // Return value
 
   LOG_D("request for %02X/%02X\n", msg.getServerID(), msg.getFunctionCode());
 
@@ -167,7 +154,8 @@ ModbusMessage ModbusClientRTU::syncRequestM(ModbusMessage msg, uint32_t token) {
     // Queue add successful?
     if (!addToQueue(token, msg, true)) {
       // No. Return error after deleting the allocated request.
-      response.setError(msg.getServerID(), msg.getFunctionCode(), REQUEST_QUEUE_FULL);
+      response.setError(msg.getServerID(), msg.getFunctionCode(),
+                        REQUEST_QUEUE_FULL);
     } else {
       // Request is queued - wait for the result.
       response = waitSync(msg.getServerID(), msg.getFunctionCode(), token);
@@ -178,18 +166,20 @@ ModbusMessage ModbusClientRTU::syncRequestM(ModbusMessage msg, uint32_t token) {
   return response;
 }
 
-// addBroadcastMessage: create a fire-and-forget message to all servers on the RTU bus
+// addBroadcastMessage: create a fire-and-forget message to all servers on the
+// RTU bus
 Error ModbusClientRTU::addBroadcastMessage(const uint8_t *data, uint8_t len) {
-  Error rc = SUCCESS;        // Return value
+  Error rc = SUCCESS; // Return value
 
   LOG_D("Broadcast request of length %d\n", len);
 
-  // We do only accept requests with data, 0 byte, data and CRC must fit into 256 bytes.
+  // We do only accept requests with data, 0 byte, data and CRC must fit into
+  // 256 bytes.
   if (len && len < 254) {
     // Create a "broadcast token"
     uint32_t token = (millis() & 0xFFFFFF) | 0xBC000000;
     ModbusMessage msg;
-    
+
     // Server ID is 0x00 for broadcast
     msg.add((uint8_t)0x00);
     // Append data
@@ -201,21 +191,21 @@ Error ModbusClientRTU::addBroadcastMessage(const uint8_t *data, uint8_t len) {
       rc = REQUEST_QUEUE_FULL;
     }
   } else {
-    rc =  BROADCAST_ERROR;
+    rc = BROADCAST_ERROR;
   }
 
   LOG_D("RC=%02X\n", rc);
   return rc;
 }
 
-
 // addToQueue: send freshly created request to queue
-bool ModbusClientRTU::addToQueue(uint32_t token, ModbusMessage request, bool syncReq) {
+bool ModbusClientRTU::addToQueue(uint32_t token, ModbusMessage request,
+                                 bool syncReq) {
   bool rc = false;
   // Did we get one?
   if (request) {
     RequestEntry re(token, request, syncReq);
-    if (requests.size()<MR_qLimit) {
+    if (requests.size() < MR_qLimit) {
       // Yes. Safely lock queue and push request to queue
       rc = true;
       LOCK_GUARD(lockGuard, qLock);
@@ -235,7 +225,8 @@ bool ModbusClientRTU::addToQueue(uint32_t token, ModbusMessage request, bool syn
 // This was created in begin() to handle the queue entries
 void ModbusClientRTU::handleConnection(ModbusClientRTU *instance) {
   // initially clean the serial buffer
-  while (instance->MR_serial->available()) instance->MR_serial->read();
+  while (instance->MR_serial->available())
+    instance->MR_serial->read();
   delay(100);
 
   // Loop forever - or until task is killed
@@ -248,44 +239,50 @@ void ModbusClientRTU::handleConnection(ModbusClientRTU *instance) {
       LOG_D("Pulled request from queue\n");
 
       // Send it via Serial
-      RTUutils::send(*(instance->MR_serial), instance->MR_lastMicros, instance->MR_interval, instance->MTRSrts, request.msg, instance->MR_useASCII);
+      RTUutils::send(*(instance->MR_serial), instance->MR_lastMicros,
+                     instance->MR_interval, instance->MTRSrts, request.msg,
+                     instance->MR_useASCII);
 
       LOG_D("Request sent.\n");
       // HEXDUMP_V("Data", request.msg.data(), request.msg.size());
 
       // For a broadcast, we will not wait for a response
-      if (request.msg.getServerID() != 0 || ((request.token & 0xFF000000) != 0xBC000000)) {
+      if (request.msg.getServerID() != 0 ||
+          ((request.token & 0xFF000000) != 0xBC000000)) {
         // This is a regular request, Get the response - if any
         ModbusMessage response = RTUutils::receive(
-          'C',
-          *(instance->MR_serial), 
-          instance->MR_timeoutValue, 
-          instance->MR_lastMicros, 
-          instance->MR_interval, 
-          instance->MR_useASCII,
-          instance->MR_skipLeadingZeroByte);
-  
-        LOG_D("%s response (%d bytes) received.\n", response.size()>1 ? "Data" : "Error", response.size());
+            'C', *(instance->MR_serial), instance->MR_timeoutValue,
+            instance->MR_lastMicros, instance->MR_interval,
+            instance->MR_useASCII, instance->MR_skipLeadingZeroByte);
+
+        LOG_D("%s response (%d bytes) received.\n",
+              response.size() > 1 ? "Data" : "Error", response.size());
         HEXDUMP_V("Data", response.data(), response.size());
-  
+
         // No error in receive()?
         if (response.size() > 1) {
           // No. Check message contents
           // Does the serverID match the requested?
           if (request.msg.getServerID() != response.getServerID()) {
             // No. Return error response
-            response.setError(request.msg.getServerID(), request.msg.getFunctionCode(), SERVER_ID_MISMATCH);
-          // ServerID ok, but does the FC match as well?
-          } else if (request.msg.getFunctionCode() != (response.getFunctionCode() & 0x7F)) {
+            response.setError(request.msg.getServerID(),
+                              request.msg.getFunctionCode(),
+                              SERVER_ID_MISMATCH);
+            // ServerID ok, but does the FC match as well?
+          } else if (request.msg.getFunctionCode() !=
+                     (response.getFunctionCode() & 0x7F)) {
             // No. Return error response
-            response.setError(request.msg.getServerID(), request.msg.getFunctionCode(), FC_MISMATCH);
-          } 
+            response.setError(request.msg.getServerID(),
+                              request.msg.getFunctionCode(), FC_MISMATCH);
+          }
         } else {
           // No, we got an error code from receive()
           // Return it as error response
-          response.setError(request.msg.getServerID(), request.msg.getFunctionCode(), static_cast<Error>(response[0]));
+          response.setError(request.msg.getServerID(),
+                            request.msg.getFunctionCode(),
+                            static_cast<Error>(response[0]));
         }
-  
+
         LOG_D("Response generated.\n");
         HEXDUMP_V("Response packet", response.data(), response.size());
 
@@ -293,7 +290,7 @@ void ModbusClientRTU::handleConnection(ModbusClientRTU *instance) {
         if (response.getError() != SUCCESS) {
           instance->errorCount++;
         }
-  
+
         // Was it a synchronous request?
         if (request.isSyncRequest) {
           // Yes. Put it into the response map
@@ -301,14 +298,14 @@ void ModbusClientRTU::handleConnection(ModbusClientRTU *instance) {
             LOCK_GUARD(sL, instance->syncRespM);
             instance->syncResponse[request.token] = response;
           }
-        // No, an async request. Do we have an onResponse handler?
+          // No, an async request. Do we have an onResponse handler?
         } else if (instance->onResponse) {
           // Yes. Call it
           instance->onResponse(response, request.token);
         } else {
           // No, but we may have onData or onError handlers
           // Did we get a normal response?
-          if (response.getError()==SUCCESS) {
+          if (response.getError() == SUCCESS) {
             // Yes. Do we have an onData handler registered?
             if (instance->onData) {
               // Yes. call it
@@ -324,7 +321,7 @@ void ModbusClientRTU::handleConnection(ModbusClientRTU *instance) {
           }
         }
       }
-      // Clean-up time. 
+      // Clean-up time.
       {
         // Safely lock the queue
         LOCK_GUARD(lockGuard, instance->qLock);
@@ -337,4 +334,4 @@ void ModbusClientRTU::handleConnection(ModbusClientRTU *instance) {
   }
 }
 
-#endif  // HAS_FREERTOS
+#endif // HAS_FREERTOS
